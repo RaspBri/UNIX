d0572f9aacb6ca2f4a80903529f72a4512706267
Brianna Murray <42558498+RaspBri@users.noreply.github.com>
2021-06-12 20:34:47 -0500

    Update function.cc

--- project2/function.cc
+++ project2/function.cc
@@ -1,10 +1,6 @@
 /*
 *Filename 	function
 *Date		2/4/21
-*Author		Brianna Murray
-*Email		bnm180003@utdallas.edu
-*Course		CS 3377.0W6 Spring 2021
-*Version	---
 *Copyright	2021, All Rights Reserved
 *
 *Description

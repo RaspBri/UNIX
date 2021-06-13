170037e49fe47c6b1444982b3892142fa25c05e2
Brianna Murray <42558498+RaspBri@users.noreply.github.com>
2021-06-12 19:50:08 -0500

    Update header3.h

--- header3.h
+++ header3.h
@@ -1,9 +1,6 @@
 /*                                                                                                                      
  *Filename       header3                                                                                                  
- *Date           2/26/21                                                                                                
- *Author         Brianna Murray                                                                                         
- *Email          bnm180003@utdallas.edu                                                                                 
- *Course         CS 3377.0U6 Spring 2021                                                                                
+ *Date           2/26/21                                                                                                                                                                              
  *Version        1.0                                                                                                    
  *Copyright      2021, All Rights Reserved                                                                              
  *                                                                                                                      
